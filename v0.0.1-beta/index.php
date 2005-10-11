59840d0da2b1f6357224ba660507a17095a90e24
klausweiss <klausweiss@391521c3-4000-0410-85a5-ec06c7e2d67b>
2005-10-11 15:05:00 +0000

    Added a folder remotely

---
+++ v0.0.1-beta/index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @category   index
+ * @package    root
+ * @author     Klaus Weiss <klaus.weiss@nanet.at>
+ * @author 
+ * 
+ * @link       http://noracmf.sourceforge.net
+ * @copyright  Klaus Weiss <klaus.weiss@nanet.at>
+ * @license:   BSD
+ *
+ * @lastchange 09.10.2005 14:38
+*/
+define('nora', true);
+
+$rootpath = './';
+require_once($rootpath.'dependence.php');
+
+?>
